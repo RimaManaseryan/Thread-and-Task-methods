16429f9f029e2c601f479d5dacace1c8b5794217
Rima <rimamanaseryan99@gmail.com>
2022-10-29 20:24:41 +0400

    tasks and threads

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -30,7 +30,84 @@ namespace Lessons
                 Console.WriteLine("Main");
             }
             Console.ReadLine();
+            Thread thread1 = new Thread(program.PrintOneByThread);
+            thread1.Start();
+
+            Task task1 = new Task(() => Console.WriteLine("Task1 is executed."));
+            task1.Start();
+            Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Task2 is executed."));
+            Task task3 = Task.Run(program.PrintOneAsync);
+            Task task4 = Task.Run(program.PrintTwoAsync);
+            #region dzer chtal
+            Console.ReadLine();
+            #endregion
         }
+
+        public void PrintOne()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(1);
+            }
+        }
+        public void PrintTwo()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(2);
+            }
+        }
+        public void PrintOneByThread()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(1);
+            }
+        }
+        public void PrintTwoByThread()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(2);
+            }
+        }
+        public void PrintOneByTask()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(1);
+            };
+        }
+        public void PrintTwoByTask()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(2);
+            };
+        }
+        public async Task PrintOneAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write(1);
+                }
+            });
+        }
+        public async Task PrintTwoAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write(2);
+                }
+            });
+        }
+    
         public void M1()
         {
             for (int i = 0; i < 20; i++)
